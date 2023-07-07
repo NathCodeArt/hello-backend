d8de6f751e64b565cbda97119eb7d8c23af03ec7
TTRIPPY <ttrippy.dessin@gmail.com>
2023-07-07 18:13:13 -0500

    segundo commit

---
+++ back/actualizar_datos.php
@@ -0,0 +1,14 @@
+<?php
+
+include '../db/conexion.php';
+
+if (isset($_POST['act'])){
+    $name = $_POST['nombre_act'];
+    $last_name = $_POST['apellidos_act'];
+    $cc = $_POST['cc'];
+
+    mysqli_query($conexion,"UPDATE usuarios SET nombre = '$name', apellido = '$last_name'
+    WHERE cedula = '$cc'");
+}
+header('location: ../datos.php')
+?>
